41f4e1fc81031e6de3bdd181483cc8870b69b688
unknown <jiminshi@usc.edu>
2024-08-28 16:42:07 +0900

    remove garbage image files

--- utils.py
+++ utils.py
@@ -1,14 +1,8 @@
 # -*- coding:utf-8 -*-
-
 import streamlit as st 
 import pandas as pd 
 
 @st.cache_data
-<<<<<<< HEAD
-def load_data():    
-    data = pd.read_csv('data/cleaned_data.csv')
-    return data
-=======
 def load_data():
     data = pd.read_csv('data/cleaned.csv')
     
@@ -20,4 +14,3 @@ def load_eda_data():
     eda_data = pd.read_csv('data/cleansed.csv')
 
     return eda_data
->>>>>>> 08fb9efad5fc2fa5fdfb22de40d0685f0638acbc
