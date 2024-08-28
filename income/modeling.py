739b6c9bb40b19ea49cea9373dfff7ed1f832ec5
unknown <jiminshi@usc.edu>
2024-08-28 09:30:30 +0900

    dataframe-ize scores + confusion matrix images output

--- income/modeling.py
+++ income/modeling.py
@@ -1,24 +1,17 @@
 
 import streamlit as st
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 from PIL import Image
 
-from sklearn.metrics import accuracy_score, roc_auc_score, precision_score, recall_score, f1_score
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import StratifiedKFold, cross_val_score
 from xgboost import XGBClassifier
 from sklearn.ensemble import RandomForestClassifier
-from collections import Counter
-import plotly.express as px
-from imblearn.under_sampling import TomekLinks, RandomUnderSampler
-from imblearn.over_sampling import SMOTE, RandomOverSampler
-from imblearn.pipeline import Pipeline, make_pipeline
+from imblearn.under_sampling import TomekLinks
 from imblearn.combine import SMOTETomek
 from xgboost import XGBClassifier
 
@@ -30,7 +23,7 @@ def evaluate_model(model, X_test, y_test, model_name):
             precision = metrics.precision_score(y_test, y_pred, average='weighted')
             recall = metrics.recall_score(y_test, y_pred, average='weighted')
             f1 = metrics.f1_score(y_test, y_pred, average='weighted')
-            roc_auc = roc_auc_score(y_test, model.predict_proba(X_test), multi_class='ovr')
+            roc_auc = metrics.roc_auc_score(y_test, model.predict_proba(X_test), multi_class='ovr')
 
             st.write(f'{model_name} 평가 지표:')
             st.write(f'Accuracy: {accuracy:.4f}')
@@ -42,26 +35,29 @@ def evaluate_model(model, X_test, y_test, model_name):
             # 혼동 행렬 시각화
             st.subheader(f'{model_name}')
             cm = metrics.confusion_matrix(y_test, y_pred)
-            plt.figure(figsize=(10, 8))
+            fig = plt.figure(figsize=(10, 8))
             sns.heatmap(cm, annot=True, fmt="d", linewidths=.5, cmap='Greens', square=True, cbar=False)
             plt.xlabel('Predicted Label')
             plt.ylabel('True Label')
             plt.title(f'{model_name} Confusion Matrixs')
             st.pyplot(plt)
-
+            fig.savefig(f'{model_name}_cm.png')
             # ROC Curve 시각화
             st.subheader(f'{model_name} ROC Curve')
             fpr, tpr, _ = metrics.roc_curve(y_test, y_pred_prob, pos_label=model.classes_[1])
-            plt.figure(figsize=(8, 6))
+            
+            fig = plt.figure(figsize=(8, 6))
             plt.plot(fpr, tpr, color='blue', label=f'ROC Curve (area = {roc_auc:.4f})')
             plt.plot([0, 1], [0, 1], color='red', linestyle='--')
             plt.xlabel('False Positive Rate')
             plt.ylabel('True Positive Rate')
             plt.title(f'{model_name} ROC Curve')
             plt.legend(loc="lower right")
+            fig.savefig(f'{model_name}_roc.png')
             st.pyplot(plt)
 
             return pd.DataFrame([{
+                    'Model': model_name,
                     'Accuracy': accuracy, 
                     'Precision': precision, 
                     'Recall': recall, 
@@ -84,7 +80,7 @@ def modeling(total_df):
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.transform(X_test)
 
-    columns = columns = ['Accuracy', 'Precision', 'Recall', 'F1 Score', 'ROC-AUC']
+    columns = columns = ['Model', 'Accuracy', 'Precision', 'Recall', 'F1 Score', 'ROC-AUC']
     scores_df = pd.DataFrame(columns=columns)
 
     with col1: 
@@ -109,9 +105,12 @@ def modeling(total_df):
         st.markdown("#### Random Forest")
         forest = RandomForestClassifier(n_estimators=200, random_state=42)
         forest.fit(X_train, y_train)
-        rfc_scores = evaluate_model(forest, X_test, y_test, 'Random Forest')
+        rfc_scores = evaluate_model(forest, X_test, y_test, 'Basic Random Forest')
         scores_df = pd.concat([scores_df, rfc_scores], ignore_index=True)
     
+    scores_df.index = scores_df['Model']
+    st.write(scores_df)
+    scores_df.to_csv('data/income_model_scores.csv')
 
     st.markdown("""
         **그래서 우리는 RFC를 쓰기로 했다... 어쩌구**
@@ -129,14 +128,19 @@ def modeling(total_df):
         smote = SMOTETomek(tomek=TomekLinks(sampling_strategy='majority'))
         X_resampled, y_resampled = smote.fit_resample(X_train, y_train)
         rfc.fit(X_resampled, y_resampled)
-        evaluate_model(forest, X_test, y_test, 'Random Forest with Sampling')
+        resampled_score = evaluate_model(forest, X_test, y_test, 'Random Forest with SMOTETomek Sampling')
+        rfc_scores = pd.concat([rfc_scores, resampled_score], ignore_index=True)
 
     with rfccol2: 
         # Balanced Random Forest
         st.markdown("#### Balanced Random Forest")
         balanced_forest = RandomForestClassifier(n_estimators=200, random_state=42, class_weight='balanced')
         balanced_forest.fit(X_train, y_train)
-        evaluate_model(balanced_forest, X_test, y_test, 'Balanced Random Forest')
+        balanced_score = evaluate_model(balanced_forest, X_test, y_test, 'Balanced Random Forest')
+        rfc_scores = pd.concat([rfc_scores, balanced_score], ignore_index=True)
+
+    st.write(rfc_scores)
+    rfc_scores.to_csv('data/income_model_rfc_scores.csv')
 
     st.markdown("""
         **그래서 우리는 그리드 서치를 했다...!!**
@@ -144,6 +148,8 @@ def modeling(total_df):
     """)
 
     rfcoptcol1, rfcoptcol2 = st.columns(2)
+    
+    optrfc_scores = pd.DataFrame(columns=columns)
     with rfcoptcol1:
         # Optimized RFC with sampling 
         rfc_opt = RandomForestClassifier(class_weight='balanced', criterion='entropy',
@@ -151,7 +157,8 @@ def modeling(total_df):
                         n_estimators=200, random_state=42)
 
         rfc_opt.fit(X_resampled, y_resampled)
-        evaluate_model(rfc_opt, X_test, y_test, 'Optimized RFC with SMOTE')
+        optsmote_rfc_scores = evaluate_model(rfc_opt, X_test, y_test, 'Optimized RFC with SMOTE')
+        optrfc_scores = pd.concat([optrfc_scores, optsmote_rfc_scores], ignore_index=True)
 
     with rfcoptcol2:
     # Optimized RFC without sampling
@@ -160,9 +167,12 @@ def modeling(total_df):
                         n_estimators=200, random_state=42)
 
         rfc_opt_nosample.fit(X_train, y_train)
-        evaluate_model(rfc_opt_nosample, X_test, y_test, 'Optimized RFC')
-        
+        opt_nosample_scores = evaluate_model(rfc_opt_nosample, X_test, y_test, 'Optimized RFC')
+        optrfc_scores = pd.concat([optrfc_scores, opt_nosample_scores], ignore_index=True)
     
+    st.write(optrfc_scores)
+    optrfc_scores.to_csv('data/income_model_opt_rfc_scores.csv')
+
     st.markdown("""### 보너스 우리 LSTM도 해봤다!!""")
      # LSTM 모델 결과 시각화 (외부 이미지로 가정)
     st.markdown("#### LSTM 딥러닝 모델")
